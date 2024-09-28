42287a2ec49b46ba71bb9e1e4eaf1fb0c0ac22c3
Filipe Prata <Filipeprata01@gmail.com>
2024-09-28 11:07:10 +0100

    testando

--- ex1.c
+++
@@ -1,8 +0,0 @@
-//program that's print your name, date_of_birth and mobile number;
-
-#include <stdio.h>
-
-int main(){
-    printf("Name: Alexandra Abramov\nDOB: July 14, 1975 \nMobile: 99-999999");
-    return 0;
-}
