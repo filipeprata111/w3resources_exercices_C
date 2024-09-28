4e78b00606c648775519857a9981a5f07efb61f1
Filipe Prata <Filipeprata01@gmail.com>
2024-09-28 11:03:09 +0100

    primeiro commit

---
+++ ex4.c
@@ -0,0 +1,15 @@
+//write a C program to print the following characters in reverse.
+
+#include <stdio.h>
+
+int main(){
+    char a = 'X';
+    char b = 'M';
+    char c = 'L';
+    printf("the reverse of %c%c%c is %c%c%c", a, b, c, c, b, a);    
+    
+    return 0;
+}
+
+
+
